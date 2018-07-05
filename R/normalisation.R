5b0f552d71e39a35d1def559ecd9b3091f4429de
zouter <wouter.saelens@gmail.com>
2018-07-05 15:23:38 +0200

    update normalisation pipeline, include more genes at the end + all plots are now ggplot (hurray!)

--- R/normalisation.R
+++ R/normalisation.R
@@ -4,25 +4,22 @@
 #'
 #' Based on \url{https://f1000research.com/articles/5-2122/v2} and \url{https://www.bioconductor.org/help/workflows/simpleSingleCell/}.
 #'
-#' @param counts The counts matrix, with genes in columns
+#' @param counts The counts matrix, with features in columns
 #' @param filter_cells Whether the cells have to be filtered
-#' @param filter_genes Whether the genes have to be filtered
-#' @param filter_hvg Whether to filter on highly variable genes
+#' @param filter_features Whether the features have to be filtered
+#' @param filter_hvg Whether to filter on highly variable features
 #' @param normalisation How to normalise
-#' @param has_spike Does this contain spike-ins, for which the gene names are preseded by ERCC
+#' @param has_spike Does this contain spike-ins, for which the feature names are preseded by ERCC
 #' @param verbose Whether to add plots
 #' @param nmads Number of median deviations for filtering outlier cells
-#' @param expressed_in_n_cells Percentage of minimal number of cells a gene has to be expressed
-#' @param min_ave_expression Minimal average expression of a gene
-#' @param hvg_fdr FDR gene filtering cutoff
-#' @param hvg_bio Biological gene filtering cutoff
-#' @param min_variable_fraction Minimal number of variable genes to retain
+#' @param min_ave_expression Minimal average expression of a feature
+#' @param hvg_fdr FDR feature filtering cutoff
+#' @param hvg_bio Biological feature filtering cutoff
+#' @param min_variable_fraction Minimal number of variable features to retain
 #'
 #' @importFrom Matrix t rowMeans
-#' @importFrom scater calculateQCMetrics isOutlier normalise plotExplanatoryVariables plotExpression plotQC nexprs
-#' @importFrom SingleCellExperiment isSpike SingleCellExperiment
-#' @importFrom BiocGenerics counts sizeFactors
-#' @importFrom Biobase pData
+#' @importFrom scater calculateQCMetrics isOutlier normalise
+#' @importFrom SingleCellExperiment isSpike SingleCellExperiment counts sizeFactors logcounts
 #' @importFrom scran computeSumFactors computeSpikeFactors trendVar decomposeVar
 #' @importFrom limSolve lsei
 #' @importFrom stats sd
@@ -31,14 +28,13 @@
 normalise_filter_counts <- function(
   counts,
   filter_cells = TRUE,
-  filter_genes = TRUE,
+  filter_features = TRUE,
   filter_hvg = TRUE,
   normalisation = "scran_size_factors",
   has_spike = any(grepl("^ERCC", colnames(counts))),
   verbose = FALSE,
   nmads = 3,
-  expressed_in_n_cells = 0.05,
-  min_ave_expression = 0.05,
+  min_ave_expression = 0.02,
   hvg_fdr = 0.05,
   hvg_bio = 0.5,
   min_variable_fraction = 0.1
@@ -51,18 +47,26 @@ normalise_filter_counts <- function(
   }
 
   normalisation_plots <- list()
+  normalisation_steps <- tibble()
 
   ########################################
   # Create data object
   ########################################
 
-  counts <- round(counts)
+  # convert to integer
+  counts <- floor(counts)
 
   sce <- SingleCellExperiment::SingleCellExperiment(assays = list(counts = Matrix::t(counts)))
 
+  # mitochondrial
   mitochondrial <- grepl("^(mt|MT|Mt)-", rownames(sce))
-  has_mito <- any(mitochondrial) && FALSE
+  has_mito <- any(mitochondrial)
 
+  # spike ins
+  spike <- grepl("^ERCC", rownames(sce))
+  has_spike <- any(spike)
+
+  # calculate qc metrics
   feature_controls <- list()
 
   if (has_mito) feature_controls$Mt <- mitochondrial
@@ -75,44 +79,36 @@ normalise_filter_counts <- function(
   )
 
   if (has_spike) {
-    is_spike <- grepl("^ERCC", rownames(sce))
-    SingleCellExperiment::isSpike(sce, "ERCC") <- is_spike
+    SingleCellExperiment::isSpike(sce, "ERCC") <- spike
   }
 
-  if (!has_mito && !has_spike) {
-    sce$scater_qc$feature_control <- sce$scater_qc$all
-  }
-
+  # plots
   if (verbose) {
     normalisation_steps <- tribble(
-      ~type, ~ngenes, ~ncells,
+      ~type, ~nfeatures, ~ncells,
       "original", nrow(sce), ncol(sce)
     )
-    print(glue::glue("Original: Genes - {nrow(sce)} Cells - {ncol(sce)}"))
-  }
+    print(glue::glue("Original: features - {nrow(sce)} Cells - {ncol(sce)}"))
 
-  if (verbose) {
-    graphics::par(mfrow = c(1,2))
-    graphics::hist(sce$scater_qc$all$total_counts/1e6, xlab = "Library sizes (millions)", main = "",
-                   breaks = 20, col = "grey80", ylab = "Number of cells")
-    graphics::hist(sce$scater_qc$all$total_features_by_counts, xlab = "Number of expressed genes", main = "",
-                   breaks = 20, col = "grey80", ylab = "Number of cells")
-    graphics::par(mfrow = c(1, 1))
-    normalisation_plots$library <- grDevices::recordPlot()
-  }
+    normalisation_plots$library <-
+      ggplot(as.data.frame(sce$scater_qc$all)) +
+      geom_histogram(aes(total_counts)) +
+      scale_x_continuous(limits = c(0, NA))
 
-  if (verbose) {
-    graphics::par(mfrow = c(2,2), mar = c(5.1, 4.1, 0.1, 0.1))
-    graphics::hist(sce$scater_qc$all$total_counts/1e6, xlab = "Library sizes (millions)", main = "",
-                   breaks = 20, col = "grey80", ylab = "Number of cells")
-    graphics::hist(sce$scater_qc$all$total_features_by_counts, xlab = "Number of expressed genes", main = "",
-                   breaks = 20, col = "grey80", ylab = "Number of cells")
-    if (has_spike) graphics::hist(sce$scater_qc$feature_control_ERCC$pct_counts, xlab = "ERCC proportion (%)",
-                                  ylab = "Number of cells", breaks = 20, main = "", col = "grey80")
-    if (has_mito) graphics::hist(sce$scater_qc$feature_control_Mt$pct_counts, xlab = "Mitochondrial proportion (%)",
-                                 ylab = "Number of cells", breaks = 20, main = "", col = "grey80")
-    graphics::par(mfrow = c(1, 1))
-    normalisation_plots$cell_quality <- grDevices::recordPlot()
+    if (has_spike) {
+      normalisation_plots$spike <-
+        ggplot(as.data.frame(sce$scater_qc$feature_control_ERCC)) +
+        geom_histogram(aes(pct_counts)) +
+        scale_x_continuous(limits = c(0, 100))
+    }
+
+    if (has_mito) {
+      normalisation_plots$mito <-
+        ggplot(as.data.frame(sce$scater_qc$feature_control_Mt)) +
+        geom_histogram(aes(pct_counts)) +
+        scale_x_continuous(limits = c(0, 100))
+    }
+
   }
 
   ########################################
@@ -141,54 +137,48 @@ normalise_filter_counts <- function(
 
     if (verbose) {
       normalisation_steps <- normalisation_steps %>%
-        add_row(type = "cell_quality_filtering", ngenes = nrow(sce), ncells = ncol(sce))
-      print(glue::glue("Cell filter: Genes - {nrow(sce)} Cells - {ncol(sce)}"))
+        add_row(type = "cell_quality_filtering", nfeatures = nrow(sce), ncells = ncol(sce))
+      print(glue::glue("Cell filter: features - {nrow(sce)} Cells - {ncol(sce)}"))
     }
   }
 
   ########################################
-  # Filter genes
+  # Filter features
   ########################################
 
-  if (filter_genes) {
+  if (filter_features) {
     ave_counts <- Matrix::rowMeans(BiocGenerics::counts(sce))
     keep <- ave_counts >= min_ave_expression
 
-    if (verbose) {
-      fontsize <- ggplot2::theme(
-        axis.text = ggplot2::element_text(size = 12),
-        axis.title = ggplot2::element_text(size = 16)
-      )
-
-      graphics::hist(log10(ave_counts), breaks = 100, main = "", col = "grey80",
-                     xlab = expression(Log[10]~"average count"))
-      graphics::abline(v = log10(min_ave_expression), col = "blue", lwd = 2, lty = 2)
-      normalisation_plots$initial_gene_filter <- grDevices::recordPlot()
-
-      print(scater::plotQC(sce, type = "highest-expression", n = 50) + fontsize)
-      normalisation_plots$top_genes_qc <- grDevices::recordPlot()
-    }
-
-    numcells <- scater::nexprs(sce, byrow = TRUE)
-    alt_keep <- numcells >= ncol(sce) * expressed_in_n_cells
-
-    if (verbose) {
-      graphics::smoothScatter(log10(ave_counts), numcells, xlab = expression(Log[10]~"average count"), ylab = "Number of expressing cells")
-      if (has_spike) {
-        is_ercc <- SingleCellExperiment::isSpike(sce, type = "ERCC")
-        graphics::points(log10(ave_counts[is_ercc]), numcells[is_ercc], col = "red", pch = 16, cex = 0.5)
-      }
-
-      normalisation_plots$cell_filtering <- grDevices::recordPlot()
-    }
-
-    if (verbose) {
-      normalisation_steps <- normalisation_steps %>%
-        add_row(type = "gene_expression_filtering", ngenes = nrow(sce), ncells = ncol(sce))
-      print(glue::glue("Gene filter: Genes - {nrow(sce)} Cells - {ncol(sce)}"))
-    }
-
     sce <- sce[keep,]
+
+    if (verbose) {
+      normalisation_plots$ave_counts <- tibble(ave_counts = ave_counts) %>%
+        ggplot() +
+        geom_histogram(aes(ave_counts)) +
+        scale_x_log10() +
+        geom_vline(xintercept = min_ave_expression)
+
+      top_features <- ave_counts %>% sort() %>% tail(20) %>% names()
+      counts_top_features <- SingleCellExperiment::counts(sce[top_features]) %>%
+        reshape2::melt(varnames = c("feature", "cell"), value.name = "count") %>%
+        dplyr::mutate(feature = factor(feature, levels = top_features))
+
+      avecounts_top_features <- counts_top_features %>%
+        group_by(feature) %>%
+        summarise(count = mean(count))
+
+      normalisation_plots$top_counts <- counts_top_features %>%
+        ggplot(aes(feature, count + 1)) +
+        geom_point(shape = "|") +
+        geom_point(data = avecounts_top_features, color = "blue", size = 4) +
+        scale_y_log10() +
+        coord_flip()
+
+      normalisation_steps <- normalisation_steps %>%
+        add_row(type = "feature_expression_filtering", nfeatures = nrow(sce), ncells = ncol(sce))
+      print(glue::glue("feature filter: features - {nrow(sce)} Cells - {ncol(sce)}"))
+    }
   }
 
   ########################################
@@ -196,21 +186,17 @@ normalise_filter_counts <- function(
   ########################################
 
   if (normalisation == "scran_size_factors") {
+    # fix for small datasets with very low number of cells
     if (ncol(sce) >= 100) {
-      # sizes <- c(20, 40, 60, 80)
-      sizes <- round(ncol(sce) / 10)
+      sizes <- c(20, 40, 60, 80)
     } else {
-      sizes <- ncol(sce)
+      sizes <- round(ncol(sce) / 5)
     }
 
-    sce <- scran::computeSumFactors(sce, sizes = sizes, positive = TRUE)
-    sce <- sce[, BiocGenerics::sizeFactors(sce) > 0] # as mentioned in the scran documentation, ensure that size factors are higher than 0
-
-    if (verbose) {
-      graphics::plot(sizeFactors(sce), sce$scater_qc$feature_control$total_counts/1e6, log = "xy",
-                     ylab = "Library size (millions)", xlab = "Size factor")
-      normalisation_plots$size_factor <- grDevices::recordPlot()
-    }
+    # In some cases, the computation of these sum factors can lead to very strange size factors, which are not at all correlated with the total counts in each cell
+    # If that is the case, we for now fall back to scater::librarySizeFactors
+    sce <- scran::computeSumFactors(sce, sizes = sizes, min.mean = 0.1, positive = TRUE)
+    sce <- sce[, SingleCellExperiment::sizeFactors(sce) > 0] # as mentioned in the scran documentation, ensure that size factors are higher than 0
 
     if(has_spike) {
       sce <- scran::computeSpikeFactors(sce, type = "ERCC", general.use = FALSE)
@@ -219,44 +205,57 @@ normalise_filter_counts <- function(
       }
     }
 
+    if (cor(sce$scater_qc$all$total_counts, sizeFactors(sce)) < 0.5) {
+      warning("Low correlation between sumFactors and total_counts, falling back to scater::librarySizeFactors")
+      SingleCellExperiment::sizeFactors(sce) <- scater::librarySizeFactors(sce)
+    }
+
     sce <- scater::normalise(sce)
+
+    if (verbose) {
+      normalisation_plots$size_factors <- tibble(
+        size_factor = sizeFactors(sce),
+        library_size = sce$scater_qc$all$total_counts
+      ) %>%
+        ggplot(aes(size_factor, library_size)) +
+        geom_point() +
+        geom_smooth(method = "lm")
+    }
   } else {
     stop("Normalisation not supported")
   }
 
   if (verbose) {
     normalisation_steps <- normalisation_steps %>%
-      add_row(type = "normalisation", ngenes = nrow(sce), ncells = ncol(sce))
-    print(glue::glue("Normalised: Genes - {nrow(sce)} Cells - {ncol(sce)}"))
+      add_row(type = "normalisation", nfeatures = nrow(sce), ncells = ncol(sce))
+    print(glue::glue("Normalised: features - {nrow(sce)} Cells - {ncol(sce)}"))
   }
 
   ########################################
-  # Select highly variable genes
+  # Select highly variable features
   ########################################
 
   if (filter_hvg) {
     var_fit <- scran::trendVar(sce, method = "spline", use.spikes = has_spike) # requires aroma.light
-    var_out <- scran::decomposeVar(sce, var_fit)
+    var_out <- scran::decomposeVar(sce, var_fit) %>% as.data.frame()
 
     if (verbose) {
-      graphics::plot(var_out$mean, var_out$total, pch = 16, cex = 0.6, xlab = "Mean log-expression",
-                     ylab = "Variance of log-expression")
-      o <- order(var_out$mean)
-      graphics::lines(var_out$mean[o], var_out$tech[o], col = "dodgerblue", lwd = 2)
-
       if (has_spike) {
-        cur_spike <- SingleCellExperiment::isSpike(sce)
-        graphics::points(var_out$mean[cur_spike], var_out$total[cur_spike], col = "red", pch = 16)
+        var_out$spike <- SingleCellExperiment::isSpike(sce)
+      } else {
+        var_out$spike <- FALSE
       }
 
-      normalisation_plots$gene_variance <- grDevices::recordPlot()
+      normalisation_plots$variance_mean <- var_out %>%
+        ggplot(aes(mean, total)) +
+          geom_point(aes(color = spike)) +
+          geom_smooth()
 
-      normalisation_plots$gene_selection <- var_out %>%
-        as("data.frame") %>%
-        ggplot2::ggplot() +
-        ggplot2::geom_point(ggplot2::aes_string("FDR", "bio")) +
-        ggplot2::geom_hline(yintercept = hvg_bio) +
-        ggplot2::geom_vline(xintercept = hvg_fdr)
+      normalisation_plots$fdr_bio <- var_out %>%
+        ggplot(aes(FDR, bio)) +
+          geom_point() +
+          geom_hline(yintercept = hvg_bio, color = "red") +
+          geom_vline(xintercept = hvg_fdr, color = "red")
     }
 
     var_out <- var_out[order(var_out$bio, decreasing = TRUE),]
@@ -265,44 +264,39 @@ normalise_filter_counts <- function(
       hvg_out <- var_out[seq(1, ceiling(min_variable_fraction * nrow(var_out))), ]
     }
 
-    # errors
-    # if (verbose & nrow(hvg_out) >= 10) {
-    #   normalisation_plots$top_genes <- scater::plotExpression(sce, rownames(hvg_out)[1:10]) + fontsize
-    #   normalisation_plots$bottom_genes <- scater::plotExpression(sce, rownames(hvg_out)[(nrow(hvg_out)-10):nrow(hvg_out)]) + fontsize
-    # }
     sce <- sce[rownames(hvg_out),]
 
     if (verbose) {
       normalisation_steps <- normalisation_steps %>%
-        add_row(type = "gene_variability_filtering", ngenes = nrow(sce), ncells = ncol(sce))
-      print(glue::glue("Variable genes filtered: Genes - {nrow(sce)} Cells - {ncol(sce)}"))
+        add_row(type = "feature_variability_filtering", nfeatures = nrow(sce), ncells = ncol(sce))
+      print(glue::glue("Variable features filtered: features - {nrow(sce)} Cells - {ncol(sce)}"))
     }
   }
 
-  expr_norm_filt <- Biobase::exprs(sce) %>% Matrix::t()
+  expr_norm_filt <- SingleCellExperiment::logcounts(sce) %>% Matrix::t()
   counts_filt <- counts[rownames(expr_norm_filt),colnames(expr_norm_filt)]
 
   ########################################
   # Iterative filtering on variability
   ########################################
   repeat {
-    gene_sds <- counts_filt %>% apply(2, stats::sd)
+    feature_sds <- counts_filt %>% apply(2, stats::sd)
     cell_sds <- counts_filt %>% apply(1, stats::sd)
 
-    genes_filtered <- which(gene_sds > 0, useNames = TRUE)
+    features_filtered <- which(feature_sds > 0, useNames = TRUE)
     cells_filtered <- which(cell_sds > 0, useNames = TRUE)
-    expr_norm_filt <- expr_norm_filt[cells_filtered, genes_filtered]
-    counts_filt <- counts_filt[cells_filtered, genes_filtered]
+    expr_norm_filt <- expr_norm_filt[cells_filtered, features_filtered]
+    counts_filt <- counts_filt[cells_filtered, features_filtered]
 
-    if((min(c(gene_sds, 1), na.rm = TRUE) > 0) && (min(c(cell_sds, 1), na.rm = TRUE) > 0)){
+    if((min(c(feature_sds, 1), na.rm = TRUE) > 0) && (min(c(cell_sds, 1), na.rm = TRUE) > 0)){
       break
     }
   }
 
   if (verbose) {
     normalisation_steps <- normalisation_steps %>%
-      add_row(type = "final_filtering", ngenes = ncol(expr_norm_filt), ncells = nrow(expr_norm_filt))
-    print(glue::glue("Final filtering: Genes - {ncol(expr_norm_filt)} Cells - {nrow(expr_norm_filt)}"))
+      add_row(type = "final_filtering", nfeatures = ncol(expr_norm_filt), ncells = nrow(expr_norm_filt))
+    print(glue::glue("Final filtering: features - {ncol(expr_norm_filt)} Cells - {nrow(expr_norm_filt)}"))
   }
 
   ########################################
